67f0aa3af1b2db18e7327c5d19092ed7730065fc
FilexGH <43278340+FilexGH@users.noreply.github.com>
2019-03-03 15:02:16 +0100

    Update bulletCars.lua

--- bulletCars.lua
+++ bulletCars.lua
@@ -5,11 +5,19 @@ local blowVehicles = {
     475
 }
 function blowFuc(weapon, ex, ey, ex, hit, sx, sy, sz)
-    if weapon == 30 or weapon == 31  then
+    if weapon == 30 or weapon == 31 then
         local model = blowVehicles[math.random(1, 4)]
+        iprint(sx, sy, sz, ex, ey, ex)
         local vehicle = createVehicle(model, sx, sy, sz)
         setElementPosition(vehicle, ex, ey, ex)
         blowVehicle(vehicle, true)
+        setTimer(
+            function()
+                destroyElement(vehicle)
+            end,
+            5000,
+            1
+        )
     end
 end
 addEventHandler("onPlayerWeaponFire", getRootElement(), blowFuc)
